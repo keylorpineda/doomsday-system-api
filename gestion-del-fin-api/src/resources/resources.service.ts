b4eb6c9f21b3d7c0f7e2a8595256748a26efc82d
keylorpineda <keylor.pineda.alvarez@est.una.ac.cr>
2026-03-04 19:01:45 -0600

    chore: remove inventory and transfers modules, including related entities, DTOs, and services

--- gestion-del-fin-api/src/resources/resources.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class ResourcesService {
-  // TODO: implementar lógica de negocio
-}
--- gestion-del-fin-api/src/transfers/transfers.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class TransfersService {
-  // TODO: implementar lógica de negocio
-}
