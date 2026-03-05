9b6dea37f881416159f17a62d650f0693b4ef89f
keylorpineda <keylor.pineda.alvarez@est.una.ac.cr>
2026-03-04 19:00:19 -0600

    chore: remove camps, explorations, and dashboard modules, including related entities, DTOs, and services

--- gestion-del-fin-api/src/dashboard/dashboard.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class DashboardService {
-  // TODO: implementar métricas del campamento
-}
--- gestion-del-fin-api/src/explorations/explorations.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class ExplorationsService {
-  // TODO: implementar lógica de negocio
-}
