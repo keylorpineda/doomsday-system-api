c7d011b4038caad188406676f72423b538954620
keylorpineda <keylor.pineda.alvarez@est.una.ac.cr>
2026-03-07 04:10:54 -0600

    feat: add transfer request endpoints and services for improved transfer management

--- src/transfers/transfers.module.ts
+++ src/transfers/transfers.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransfersController } from './transfers.controller';
 import { TransfersService } from './transfers.service';
+import { RequestsService } from './services/requests.service';
+import { ApprovalsService } from './services/approvals.service';
+import { TransferExecutionService } from './services/transfer-execution.service';
 import { IntercampRequest } from './entities/intercamp-request.entity';
 import { RequestResourceDetail } from './entities/request-resource-detail.entity';
 import { RequestPersonDetail } from './entities/request-person-detail.entity';
 import { Approval } from './entities/approval.entity';
+import { Camp } from '../camps/entities/camp.entity';
+import { Person } from '../users/entities/person.entity';
+import { UserAccount } from '../users/entities/user-account.entity';
+import { Inventory } from '../resources/entities/inventory.entity';
+import { Resource } from '../resources/entities/resource.entity';
+import { InventoryMovement } from '../resources/entities/inventory-movement.entity';
+import { AuditLog } from '../common/entities/audit-log.entity';
 
 @Module({
   imports: [
@@ -14,10 +24,22 @@ import { Approval } from './entities/approval.entity';
       RequestResourceDetail,
       RequestPersonDetail,
       Approval,
+      Camp,
+      Person,
+      UserAccount,
+      Inventory,
+      Resource,
+      InventoryMovement,
+      AuditLog,
     ]),
   ],
   controllers: [TransfersController],
-  providers: [TransfersService],
+  providers: [
+    TransfersService,
+    RequestsService,
+    ApprovalsService,
+    TransferExecutionService,
+  ],
   exports: [TransfersService],
 })
 export class TransfersModule {}
